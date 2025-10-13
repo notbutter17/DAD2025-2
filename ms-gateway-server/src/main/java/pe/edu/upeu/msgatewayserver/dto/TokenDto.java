df0acb3605514eb5127973f07be765082cadc9fd
notbutter17 <lipaj7687@gmail.com>
2025-10-13 11:24:17 -0500

    nuevo

---
+++ ms-gateway-server/src/main/java/pe/edu/upeu/msgatewayserver/dto/TokenDto.java
@@ -0,0 +1,15 @@
+package pe.edu.upeu.msgatewayserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Data
+public class TokenDto {
+    private String token;
+}
+
